7300a8b950fc7c470e57bf9a77ad835592493e47
Eliron <tkhruirjhnh@gmail.com>
2022-01-06 21:55:15 +0200

    bag in loadfxml because new if

--- src/main/java/il/ac/haifa/ClinicSystem/ClientMenuController.java
+++ src/main/java/il/ac/haifa/ClinicSystem/ClientMenuController.java
@@ -5,33 +5,15 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 
 public class ClientMenuController {
-
-
-
-    @FXML
-    private ResourceBundle resources;
-
-    @FXML
-    private URL location;
 
     @FXML
     private Button doctorBTN;
 
-
-
     @FXML
     void clickDoctor(ActionEvent event) throws IOException {
         App.setRoot("doctorList");
-    }
-
-    @FXML
-    void initialize() {
-        assert doctorBTN != null : "fx:id=\"doctorBTN\" was not injected: check your FXML file 'clientMenu.fxml'.";
-
     }
 
     private SimpleClient chatClient;
@@ -39,7 +21,4 @@ public class ClientMenuController {
     public void setClient(SimpleClient c) {
         this.chatClient = c;
     }
-
-
-
 }
--- src/main/java/il/ac/haifa/ClinicSystem/DoctorListController.java
+++ src/main/java/il/ac/haifa/ClinicSystem/DoctorListController.java
@@ -1,6 +1,17 @@
 package il.ac.haifa.ClinicSystem;
 
-public class DoctorListController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public  class DoctorListController  {
+
+@FXML
+private Button b;
+
+@FXML
+void vvdvdvv(ActionEvent event) {
+ }
     private SimpleClient chatClient;
     public void setClient(SimpleClient c) {
         this.chatClient = c;
