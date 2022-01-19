3b49836dc15d0eb337f351d6d48bfdf2a5efd3ee
Eliron <tkhruirjhnh@gmail.com>
2022-01-19 16:46:13 +0200

    a little design

--- src/main/java/il/ac/haifa/ClinicSystem/ClientMenuController.java
+++ src/main/java/il/ac/haifa/ClinicSystem/ClientMenuController.java
@@ -3,7 +3,10 @@ package il.ac.haifa.ClinicSystem;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 
+import javafx.scene.image.ImageView ;
 import java.io.IOException;
 
 public class ClientMenuController {
@@ -11,10 +14,25 @@ public class ClientMenuController {
     private SimpleClient chatClient;
 
     @FXML
+    private AnchorPane dwon;
+
+    @FXML
     private Button exitBtn;
 
     @FXML
+    private AnchorPane midlle;
+
+    @FXML
     private Button orderBtn;
+
+    @FXML
+    private ImageView picture;
+
+    @FXML
+    private Text txet;
+
+    @FXML
+    private AnchorPane up;
 
     @FXML
     private Button viewBtn;
